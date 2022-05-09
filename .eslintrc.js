cbd2bf0ef160eb9b5c451a018deb3a4725046a9a
limit-b <jehan082@gmail.com>
2022-05-10 02:39:57 +0900

    Chore: Add setup for prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: { 'prettier/prettier': 'off' },
+};
