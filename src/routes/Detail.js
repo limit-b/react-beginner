87cbda22d818594bd459b0ab5e4ec97e7051b52a
limit-b <jehan082@gmail.com>
2022-06-28 03:55:16 +0900

    Feat: Add useEffect to prevent manual access

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,21 @@
-function Detail(props) {
-    // eslint-disable-next-line no-console
-    console.log(props);
-    return <span>hello</span>;
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function Detail() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.state === null) {
+            navigate('/');
+        }
+    });
+
+    if (location.state) {
+        const { title } = location.state;
+        return <span>{title}</span>;
+    }
+    return null;
 }
 
 export default Detail;
